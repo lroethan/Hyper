1368b4e0375cc102abb0d68959a744ab20191eee
qw4990 <zhangyuanjia@pingcap.com>
2023-11-06 19:28:47 +0800

    fixup

--- index_selection_evaluation/tests/test_tidb.py
+++ index_selection_evaluation/tests/test_tidb.py
@@ -11,50 +11,50 @@ class TestTiDB(unittest.TestCase):
         db = TiDBDatabaseConnector2("test")
         db.close()
 
-    # def test_tidb_creating(self):
-    #     db = TiDBDatabaseConnector2("test")
-    #     db.exec_fetch("drop table if exists t")
-    #     db.exec_fetch("create table t (a int, b int, c int)")
-    #     oid = db.hypo_create_secondary_index("t", ["a", "b", "c"])
-    #     print("OID: ", oid)
-    #     db.hypo_delete_single_physical_design(oid)
-    #     oid = db.hypo_create_columnstore_index("t")
-    #     db.hypo_delete_single_physical_design(oid)
+    def test_tidb_creating(self):
+        db = TiDBDatabaseConnector2("test")
+        db.exec_fetch("drop table if exists t")
+        db.exec_fetch("create table t (a int, b int, c int)")
+        oid = db.hypo_create_secondary_index("t", ["a", "b", "c"])
+        print("OID: ", oid)
+        db.hypo_delete_single_physical_design(oid)
+        oid = db.hypo_create_columnstore_index("t")
+        db.hypo_delete_single_physical_design(oid)
 
-    #     db.hypo_create_secondary_index("t", ["a", "b", "c"])
-    #     db.hypo_create_secondary_index("t", ["a", "b", "c"])
-    #     db.hypo_create_secondary_index("t", ["a", "b", "c"])
-    #     db.hypo_create_columnstore_index("t")
-    #     db.hypo_delete_all_physical_designs()
+        db.hypo_create_secondary_index("t", ["a", "b", "c"])
+        db.hypo_create_secondary_index("t", ["a", "b", "c"])
+        db.hypo_create_secondary_index("t", ["a", "b", "c"])
+        db.hypo_create_columnstore_index("t")
+        db.hypo_delete_all_physical_designs()
         
-    #     db.exec_fetch("drop table t")
-    #     db.close()
+        db.exec_fetch("drop table t")
+        db.close()
 
-    # def test_tidb_storage(self):
-    #     db = TiDBDatabaseConnector2("test")
-    #     # db.exec_only("drop table if exists tt")
-    #     # db.exec_only("create table tt (a int, b int, c int)")
-    #     # db.exec_only("insert into tt values (1, 1, 1), (2, 2, 2)")
-    #     # db.exec_only("analyze table tt")
-    #     oid = db.hypo_create_secondary_index("tt", ["a", "b"])
-    #     storage = db.get_storage_single(oid)
-    #     print(">> ", storage)
+    def test_tidb_storage(self):
+        db = TiDBDatabaseConnector2("test")
+        # db.exec_only("drop table if exists tt")
+        # db.exec_only("create table tt (a int, b int, c int)")
+        # db.exec_only("insert into tt values (1, 1, 1), (2, 2, 2)")
+        # db.exec_only("analyze table tt")
+        oid = db.hypo_create_secondary_index("tt", ["a", "b"])
+        storage = db.get_storage_single(oid)
+        print(">> ", storage)
 
-    #     oid = db.hypo_create_secondary_index("tt", ["a", "b", "c"])
-    #     storage = db.get_storage_single(oid)
-    #     print(">> ", storage)
+        oid = db.hypo_create_secondary_index("tt", ["a", "b", "c"])
+        storage = db.get_storage_single(oid)
+        print(">> ", storage)
 
-    #     oid = db.hypo_create_columnstore_index("tt")
-    #     storage = db.get_storage_single(oid)
-    #     print(">> ", storage)
+        oid = db.hypo_create_columnstore_index("tt")
+        storage = db.get_storage_single(oid)
+        print(">> ", storage)
 
-    #     # db.exec_fetch("drop table tt")
-    #     pass
+        # db.exec_fetch("drop table tt")
+        pass
 
-    # def test_tidb_plan_cost(self):
-    #     db = TiDBDatabaseConnector2("test")
-    #     print(db.get_plan_single("select * from tt where a=1"))
-    #     print(db.get_cost_single("select * from tt where a=1"))
+    def test_tidb_plan_cost(self):
+        db = TiDBDatabaseConnector2("test")
+        print(db.get_plan_single("select * from tt where a=1"))
+        print(db.get_cost_single("select * from tt where a=1"))
 
     def test_tidb_real_physical(self):
         db = TiDBDatabaseConnector2("test")
